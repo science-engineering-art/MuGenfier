ec9fff7bc421092939ae500dc97db24ebaa9d169
Marcos <marcosmath44@gmail.com>
2023-06-06 04:55:41 -0400

    first attempt with model of embeddings
    first attempt with model of embeddings

--- src/model/new_model.py
+++ src/model/new_model.py
@@ -1,5 +1,7 @@
 import json
+from multiprocessing.dummy import active_children
 import os
+from pickletools import optimize
 import random
 import shutil
 import zipfile
@@ -9,6 +11,7 @@ import cv2
 import matplotlib.pyplot as plt
 from pandas import DataFrame
 # from sklearn.model_selection import train_test_split, KFold
+import keras as kr
 from keras import Sequential
 import tensorflow as tf
 from tensorflow.keras.models import Model
@@ -115,5 +118,38 @@ X_test, Y_test = load_data('./dataset/split/tests/features', GENRES)
 X_val, Y_val = load_data('./dataset/split/validation/features', GENRES)
 
 
+nn = [386 + 500, 512, 256, 10]
+
+model = Sequential()
+model.add(Dense(nn[1], activation='relu'))
+model.add(Dense(nn[2], activation='relu'))
+model.add(Dense(nn[3], activation='softmax'))
+
+# model.compile(loss = 'mse', optimizer = kr.optimizer.SGD(lr = 0.05), metrics = ['acc'])
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 
+history = []
+
+model_storage = './ebddmodel'
+try:
+    os.mkdir(model_storage)
+except:
+    pass
+
+if 'model_storage' not in os.listdir(model_storage):
+    os.mkdir(f'{model_storage}/model_storage')
+
+last_iter = 0
+
+if len(os.listdir(f'{model_storage}/model_storage')) > 0:
+    last_iter = sorted([int(model.removeprefix('model_').removesuffix('.keras'))
+        for model in os.listdir(f'{model_storage}/model_storage')], reverse=True)[0]
+    model = tf.keras.saving.load_model(f'{model_storage}/model_storage/model_{last_iter}.keras')
+
+for i in range(last_iter, 50):
+    history.append(model.fit(x=X_train, y=Y_train, epochs=10, validation_data=(X_val, Y_val)))
+    model.save(f'{model_storage}/model_storage/model_{i}.keras')
+    
+    
+# print(model.summary())
