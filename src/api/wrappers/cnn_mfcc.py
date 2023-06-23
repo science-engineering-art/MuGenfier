92bfeabc71cf1ddafd7faa01ae36d65c4e65e0bf
FreeVisitant <128440940+FreeVisitant@users.noreply.github.com>
2023-06-22 18:03:41 -0600

    Update cnn_mfcc.py

--- src/api/wrappers/cnn_mfcc.py
+++ src/api/wrappers/cnn_mfcc.py
@@ -1,5 +1,12 @@
 from wrappers.model import Model
 from typing import Any
+import os
+import librosa
+import numpy as np
+from PIL import Image
+from keras.models import load_model
+from PIL import Image
+import numpy as np
 
 
 # Raul model
@@ -9,13 +16,26 @@ class CNN_MFCC(Model):
         super().__init__(model_path)
         
     def load(self, model_path: str):
+        model_path = os.path.join(, f"{model_name}.h5")
+        loaded_model = load_model(model_path)
+        return loaded_model
         # missing implementation
-        ...
+        
+        
         
     def extract_feature(song_path:str) -> Any:
         # missing implementation
-        ...
+        x , sr = librosa.load(audio_path)
+        mfccs = librosa.feature.mfcc(y=x, sr=sr)
+        img_array = np.array(mfccs)
+        img_array = img_array / 255.0  # Normalización de los datos
+        img_array = np.expand_dims(img_array, axis=0)
+        return img_array
         
     def predict(song_path: str) -> str:
         # missing implementation
-        return "classic"
+        prediction = loaded_model.predict(img_array)
+        genres = ['blues', 'classical', 'country', 'disco', 'hiphop', 'jazz', 'metal', 'pop', 'reggae', 'rock']
+        genre = genres[np.argmax(prediction)] 
+
+        return { "genre": genre }
