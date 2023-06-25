866f27698f1d5a8087d02d8fa510f1db0fd1596d
leandro-driguez <leandro_driguez@outlook.com>
2023-06-25 12:02:10 -0400

    refactor: move method to utils

--- mugenfier/utils/wave.py
+++ mugenfier/utils/wave.py
@@ -2,8 +2,7 @@ from scipy.io import wavfile
 
 def load_wav(filename: str):
     wav = wavfile.read(filename)
-    wav = wav[1]/2.0**15
-    return wav
+    return wav[1]/2.0**15, wav[0]
 
 def get_wav_path(path: str, genre: str, index: int):
     wav_path = f'{path}/{genre}/{genre}.000{index}.wav'
